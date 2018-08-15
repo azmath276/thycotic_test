c65735458d7e58462c29404d93179777d8c2a1cc
Azmath Ikram <iazmath@ymail.com>
2018-08-15 08:48:44 +0530

    Update main method

--- Thycotic_Test/Program.cs
+++ Thycotic_Test/Program.cs
@@ -10,19 +10,7 @@ namespace Thycotic_Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Azmath");
-            PriceDeterminator priceDeterminator = new PriceDeterminator();
-            Car car = new Car
-            {
-                AgeInMonths = 3 * 12,
-                NumberOfCollisions = 0,
-                NumberOfMiles = 250000,
-                NumberOfPreviousOwners = 1,
-                PurchaseValue = 35000m
-            };
-
-            priceDeterminator.DetermineCarPrice(car);
-
+            Console.WriteLine("Hello Thycotic!");
             Console.ReadLine();
         }
     }
